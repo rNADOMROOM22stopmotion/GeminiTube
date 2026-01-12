5d6152461b914e6d8769f223e5d0f35a2799c6c0
dhruv <senpaigaming126@gmail.com>
2026-01-13 01:56:03 +0530

    updated README and .gitignore

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Simple usage guide:
 
 1. Make a folder "tokens" and add oauth2 tokens inside oauth.json file inside this folder.
 2. Get hold of a youtube video id. You may use the get_video_list() function located in utils/videos.py.
-   This acutally returns video_id_list (used by other functions) and data. Here, data basically contains the API reponse. This will contain 50 items/ videos.
+   This actually returns video_id_list (used by other functions) and data. Here, data basically contains the API response. This will contain 50 items/ videos.
 3. Choose any video from these 50. Copy that video's ID inside utils/video.csv below "lastvideoid". All the videos above this video will be updated.
 4. Note that this is only temporary. Whenever you upload new videos and run this script videos.csv will be auto updated.
 5. Next, run the main.py file.
--- models.py
+++ models.py
@@ -27,7 +27,7 @@ class VideoBody(BaseModel):
 class GeminiOutput(BaseModel):
     title: str = Field(description='Title of the video, optimized for best CTR but not too different/ changed meaning from original. The title should be english first, then gujarati. title must be less than 100 letters',
                        max_length=100)
-    description: str = Field(description='Description of the youtube video. Keep the first paragraph reserved for Ingredients and their quantity. You may use relevent emojis and newline characters after each ingredient. After ingredients, leave 2 new lines and generate a 45-60 paragraph related/ relevent to the video recipe. After that leave 1 more new line and give link to my instagram with required title (e.g. Instagram: https://www.instagram.com/p/CuWxeGhM416/). Finally, leave 3 more new lines and generate about 10 hashtags related to the content optimized for maximum reach.')
+    description: str = Field(description='Description of the youtube video. Keep the first paragraph reserved for Ingredients and their quantity. You may use relevent emojis and newline characters after each ingredient. After ingredients, leave 2 new lines and generate a 45-60 paragraph related/ relevent to the video recipe. After that leave 1 more new line and give link to my instagram with required title (e.g. Instagram: https://www.instagram.com/p//). Finally, leave 3 more new lines and generate about 10 hashtags related to the content optimized for maximum reach.')
     tags: list[str] = Field(description='from the provided titles and description, generate 15 relevent tags for youtube.')
 
 class GeminiInput(BaseModel):
